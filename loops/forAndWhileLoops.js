81260766c24bb4c4bcf811386d03035c771a87e7
Mich Seixas <mcseixas@gmail.com>
2023-07-12 12:22:21 +0200

    loops and conditionals in Introduction to JavaScript

---
+++ loops/forAndWhileLoops.js
@@ -0,0 +1,41 @@
+for (var i=1 ; i <= 3 ; i++) {
+    console.log(i);
+}
+console.log ("Go!")
+
+console.log("----")
+
+for (var i=10 ; i >=0 ; i--) {
+    console.log(i);
+}
+console.log ("Happy New Year!")
+
+console.log("----")
+
+var i =10
+while (i>=0) {
+    console.log(i);
+    i--;
+} console.log ("Happy New Year!")
+
+console.log("----")
+
+for (var i=1; i<=5; i++){
+    console.log(i);
+} console.log("Counting completed!")
+
+console.log("----")
+
+var i=5
+while (i>=0){
+    console.log(i);
+    i--
+} console.log("Counting finished")
+
+console.log("----")
+
+var i=2018
+while (i<=2022){
+    console.log(i)
+    i++
+}
